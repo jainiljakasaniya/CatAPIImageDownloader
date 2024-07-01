290f5cc2e24b61c71937146d754e6304daf256f2
jainil jakasaniya <jainiljakasaniya@gmail.com>
2024-07-01 15:51:53 +0530

    change lib to axios

--- index.js
+++ index.js
@@ -1,58 +1,67 @@
-const client = require("https");
-const server = require("http").createServer();
-
+const axios = require("axios");
+const https = require("https");
+const http = require("http").createServer();
 const fs = require("fs");
-const rp = require("request-promise");
+const path = require("path");
 
 const hostname = "127.0.0.1";
 const port = 8000;
 
 async function loadImage() {
   try {
-    // https://documenter.getpostman.com/view/4016432/RWToRJCq
-    let url = `https://api.thecatapi.com/v1/images/search`;
-    const response = await rp.get(url, {
-      qs: {
-        mime_types: "png",
+    const url = "https://api.thecatapi.com/v1/images/search";
+    const response = await axios.get(url, {
+      params: {
+        mime_types: "gif",
+        // mime_types: "png",
         limit: 10,
       },
     });
-    return JSON.parse(response);
+    return response.data;
   } catch (e) {
-    console.log(e);
+    console.error("Error fetching images:", e);
   }
 }
 
 function downloadImage(url, filepath) {
   return new Promise((resolve, reject) => {
-    client.get(url, (res) => {
+    https.get(url, (res) => {
       if (res.statusCode === 200) {
-        res
-          .pipe(fs.createWriteStream(filepath))
+        res.pipe(fs.createWriteStream(filepath))
           .on("error", reject)
           .once("close", () => resolve(filepath));
       } else {
-        reject(
-          new Error(`Request Failed With a Status Code: ${res.statusCode}`)
-        );
+        reject(new Error(`Request Failed With Status Code: ${res.statusCode}`));
       }
     });
   });
 }
 
-server
-  .on("request", async (req, res) => {
-    if (req.url != "/favicon.ico") {
-      let images = await loadImage();
-      for (let cat of images) {
-        let fileUrl = cat.url;
-        let filepath =
-          "./catImg/" + fileUrl.substring(fileUrl.lastIndexOf("/") + 1);
-        downloadImage(fileUrl, filepath).then(console.log).catch(console.error);
+http.on("request", async (req, res) => {
+  if (req.url !== "/favicon.ico") {
+    try {
+      const images = await loadImage();
+      if (images && images.length > 0) {
+        const downloadPromises = images.map((cat) => {
+          const fileUrl = cat.url;
+          const filepath = path.join(__dirname, "catImg", path.basename(fileUrl));
+          return downloadImage(fileUrl, filepath).then(() => {
+            console.log(`Saved: ${filepath}`);
+            return filepath;
+          });
+        });
+
+        await Promise.all(downloadPromises);
+        res.end("Cat Images are saved in ./catImg/");
+      } else {
+        res.end("No cat images found.");
       }
-      res.end("Cat Images is Saved on ./catImages/");
+    } catch (error) {
+      console.error("Error processing request:", error);
+      res.statusCode = 500;
+      res.end("Internal Server Error");
     }
-  })
-  .listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-  });
+  }
+}).listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
